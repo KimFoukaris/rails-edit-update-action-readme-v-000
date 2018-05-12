78f0b80c5b40603ce4ba4ca3b05f51be09cadb44
Kim Foukaris <kimfoukaris@outlook.com>
2018-05-12 14:04:55 +0000

    automatically backed up by learn

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,28 @@
+class PostsController < ApplicationController
+	def index
+		@posts = Post.all
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+	  @post = Post.new
+	  @post.title = params[:title]
+	  @post.description = params[:description]
+	  @post.save
+	  redirect_to post_path(@post)
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+	end
+end
